5a03700a6e33f36db0b61b2cd9e03e16b0e78d88
FOREST <forest310125@gmail.com>
2025-03-11 20:33:56 -0600

    Update config.tsx

--- src/App/config.tsx
+++ src/App/config.tsx
@@ -21,7 +21,7 @@ export const config: Config = {
       display: 'Resume',
       ariaLabel: 'Resume in Google Drive (opens in new window)',
       icon: <Resume />,
-      href: 'https://drive.google.com/file/d/1BToXANbjk16TSVjhcIfV8aKlpF-xePZl/view?usp=sharing',
+      href: 'https://drive.google.com/file/d/1vOPvb_G68OPUC-jCs_NQrfiUMuWoNzdj/view?usp=sharing',
     },
     {
       name: 'email',
